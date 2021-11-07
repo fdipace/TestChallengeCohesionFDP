5ebc644ab0e1f72c67666132a10d128f756d1466
fdipace <franco.dipace@cmegroup.com>
2021-11-07 20:31:00 -0300

    Adding second scenario, validating that the amount of results and pages are as expected.

--- TestChallengeCohesionFDP/TestChallengeCohesionTests.cs
+++ TestChallengeCohesionFDP/TestChallengeCohesionTests.cs
@@ -32,5 +32,27 @@ namespace TestChallengeCohesionFDP
 
             }
         }
+
+        [TestMethod]
+        public void Story2()
+        {
+            string stationName = "63rd Street Weather Station";
+            var client = new RestClient(baseURL);
+
+            var request = new RestRequest("?$where=measurement_timestamp between '2019-01-01' and '2019-12-31'&station_name=" + stationName + "&$limit=10&$offset=0", Method.GET);
+            var response = client.Execute(request);
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<List<Dictionary<string, string>>>(response);
+
+            foreach (var item in output)
+            {
+                var resultStationName = item["station_name"];
+                var resultMeasurementId = item["measurement_id"];
+
+                Assert.IsTrue(resultStationName.Equals(stationName), "Station Name '" + resultStationName + "' does not match with expected station name '." + stationName + "'");
+                Assert.IsTrue(resultMeasurementId.ToLower().Contains(stationName.ToLower().Replace(" ", "")), "Measurement Station Id '" + resultMeasurementId + "' does not correspond with expected Measurement Id for Station Name '." + stationName + "'");
+
+            }
+        }
     }
 }
