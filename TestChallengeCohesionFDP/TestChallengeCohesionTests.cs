355d83dda60e5a9a3bd4247dbab01d23ce2a70dc
fdipace <franco.dipace@cmegroup.com>
2021-11-07 20:39:30 -0300

    Adding second foreach loop so I can compare every item from page 1 against every item of page 2, to be sure there is no repetition.

--- TestChallengeCohesionFDP/TestChallengeCohesionTests.cs
+++ TestChallengeCohesionFDP/TestChallengeCohesionTests.cs
@@ -39,18 +39,27 @@ namespace TestChallengeCohesionFDP
             string stationName = "63rd Street Weather Station";
             var client = new RestClient(baseURL);
 
-            var request = new RestRequest("?$where=measurement_timestamp between '2019-01-01' and '2019-12-31'&station_name=" + stationName + "&$limit=10&$offset=0", Method.GET);
-            var response = client.Execute(request);
+            var request1 = new RestRequest("?$where=measurement_timestamp between '2019-01-01' and '2019-12-31'&station_name=" + stationName + "&$limit=10&$offset=0", Method.GET);
+            var request2 = new RestRequest("?$where=measurement_timestamp between '2019-01-01' and '2019-12-31'&station_name=" + stationName + "&$limit=10&$offset=1", Method.GET);
+            var response1 = client.Execute(request1);
+            var response2 = client.Execute(request2);
             var deserialize = new JsonDeserializer();
-            var output = deserialize.Deserialize<List<Dictionary<string, string>>>(response);
+            var output1 = deserialize.Deserialize<List<Dictionary<string, string>>>(response1);
+            var output2 = deserialize.Deserialize<List<Dictionary<string, string>>>(response2);
 
-            foreach (var item in output)
+            foreach (var a in output1)
             {
-                var resultStationName = item["station_name"];
-                var resultMeasurementId = item["measurement_id"];
+                var resultStationNameA = a["station_name"];
+                var resultMeasurementIdA = a["measurement_id"];
 
-                Assert.IsTrue(resultStationName.Equals(stationName), "Station Name '" + resultStationName + "' does not match with expected station name '." + stationName + "'");
-                Assert.IsTrue(resultMeasurementId.ToLower().Contains(stationName.ToLower().Replace(" ", "")), "Measurement Station Id '" + resultMeasurementId + "' does not correspond with expected Measurement Id for Station Name '." + stationName + "'");
+                foreach (var b in output2)
+                {
+                    var resultStationNameB = b["station_name"];
+                    var resultMeasurementIdB = b["measurement_id"];
+
+                    Assert.IsTrue(resultStationNameA.Equals(resultStationNameB), "Station Name '" + resultStationNameA + "' does not match with expected station name '." + resultStationNameB + "'");
+                    Assert.IsFalse(resultMeasurementIdA.ToLower().Equals(resultMeasurementIdB.ToLower()), "Measurement Station Id '" + resultMeasurementIdA + "' in page 1 is equal to Measurement Id '." + resultMeasurementIdB + "' on page 2");
+                }
 
             }
         }
